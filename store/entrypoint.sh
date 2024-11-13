6d6a2f33fe1d1c264b59233b57aa4f823ebe14c7
Illa Tsiluiko <helperfor@ukr.net>
2024-11-13 12:21:23 +0200

    add docker-compose

--- store/entrypoint.sh
+++ store/entrypoint.sh
@@ -2,13 +2,13 @@
 echo "Running from entrypoint.sh"
 
 # Run all migrations
-python3 manage.py migrate
+python manage.py migrate
 
 # Collect static files
 #python3 src/manage.py collectstatic --noinput
 
 # Create superuser
-python3 manage.py create_superuser
+python manage.py create_superuser
 
 # Start the server
-python3 manage.py runserver 0.0.0.0:8000
+python manage.py runserver 0.0.0.0:8000
