12ddd089eaeee11a6b9567093a0bcf175d8687a3
Illa Tsiluiko <helperfor@ukr.net>
2024-11-17 17:54:10 +0200

    fix dockerfile

--- store/entrypoint.sh
+++ store/entrypoint.sh
@@ -4,11 +4,14 @@ echo "Running from entrypoint.sh"
 # Run all migrations
 python manage.py migrate
 
+python manage.py loaddata products/fixtures/categories.json
+python manage.py loaddata products/fixtures/products.json
+
 # Collect static files
 #python3 src/manage.py collectstatic --noinput
 
 # Create superuser
-python manage.py create_superuser
+python manage.py createsuperuser
 
 # Start the server
 python manage.py runserver 0.0.0.0:8000
